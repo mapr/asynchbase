5cf6d735392c271f1c3026b27329c080536d25f2
Aditya Kishore <aditya@maprtech.com>
2014-09-30 11:05:40 -0700

    MAPR-15454: Performance issues with incrementColumnValue call
    Conflicts:
    
    	src/HBaseClient.java

--- src/HBaseClient.java
+++ src/HBaseClient.java
@@ -549,7 +549,7 @@ public final class HBaseClient {
 
     // For MapR
     conf = new Configuration();
-    mPool = new MapRThreadPool();
+    mPool = new MapRThreadPool(conf);
     mTableMappingRules = new MapRTableMappingRules(conf);
     flushOnPut = conf.getBoolean(CONFIG_PARAM_FLUSH_ON_PUT, false);
   }
