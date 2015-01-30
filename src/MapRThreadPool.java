20552d8808e0f6addafcd3a967842d8da76d63a2
Bharat Baddepudi <bbaddepudi@maprtech.com>
2015-01-30 15:33:21 -0800

    MAPR 16983: Remove references to old MapRResult APIs.
    Pending review by Aditya. Tested by ensuring failing test runs ok with fix.

--- src/MapRThreadPool.java
+++ src/MapRThreadPool.java
@@ -423,8 +423,7 @@ public class MapRThreadPool implements com.mapr.fs.jni.MapRCallBackQueue {
           for (int i = 0 ;i < num_rows; ++i) {
             int keyLen = mresults[i].getKeyLength();
             byte [] key = new byte[keyLen];
-            mresults[i].getByteBuf().position(0);
-            mresults[i].getByteBuf().get(key, 0, keyLen);
+            System.arraycopy(mresults[i].bufBytes, 0, key, 0, mresults[i].getKeyLength());
             ArrayList<KeyValue> kv =
                 MapRConverter.toAsyncHBaseResult(mresults[i], key, mTable);
             rows.add(kv);
