164fd06474fdab77b7477f484ac0f1e45426b8ab
Arjun Mahesh <amahesh@maprtech.com>
2021-12-10 13:33:29 -0800

    Update git-to-git.sh

--- devops/git-to-git.sh
+++ devops/git-to-git.sh
@@ -111,7 +111,8 @@ git@github.com:confluentinc/kafka-connect-jdbc.git git@github.com:mapr/private-k
 git@github.com:confluentinc/kafka-connect-storage-common.git git@github.com:mapr/kafka-connect-storage-common.git kafka-storage-common
 git@github.com:confluentinc/kafka-connect-storage-common.git git@github.com:mapr/private-kafka-connect-storage-common.git private-kafka-storage-common
 git@github.com:confluentinc/kafka-connect-hdfs.git git@github.com:mapr/private-kafka-connect-hdfs.git private-kafka-connect-hdfs
-git@github.com:apache/log4j.git git@github.com:mapr/private-log4j.git private-log4j"
+git@github.com:apache/log4j.git git@github.com:mapr/private-log4j.git private-log4j
+git@github.com:apache/log4j-extras.git git@github.com:mapr/private-log4j-extras.git private-log4j-extras"
 
 if [ ${TEST} -eq 1 ]; then
     TUPLES="git@github.com:OpenTSDB/asynchbase.git git@github.com:mapr/asynchbase.git asynchbase"
